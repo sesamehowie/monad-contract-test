c57a5d617ee14383c5e1be5b257aac87854aef1d
sesamehowie <spooolieyeah@gmail.com>
2025-04-25 18:07:49 +0300

    chest test

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from tests.rpsTest import test_rps
 from tests.rouletteTest import test_roulette
+from tests.chestTest import test_chest
 
 
 def main():
-    choice = int(input("1. Test RPS\n2. Test Roulette\n"))
+    choice = int(input("1. Test RPS\n2. Test Roulette\n3. Test Chest\n"))
 
     match choice:
         case 1:
             return test_rps()
         case 2:
             return test_roulette()
+        case 3:
+            return test_chest()
         case _:
             return
 
---
+++ tests/chestTest.py
@@ -0,0 +1,32 @@
+import time
+from src.client.eth_client import EthClient
+from config import ADMIN_PKEY
+from src.network.network import Monad
+from src.interfaces.interfaces import ChestsInterface
+
+FUNCTIONS_TO_TEST = [
+    {"name": "withdraw", "args": [int(591 * 10**18)]},
+]
+
+
+def test_chest():
+    client = EthClient(
+        account_name="admin wallet", private_key=ADMIN_PKEY, network=Monad
+    )
+    interface = ChestsInterface
+
+    for item in FUNCTIONS_TO_TEST:
+        fn_name = item["name"]
+        arg = item["args"]
+
+        try:
+            interface.execute_write_function(
+                function_name=fn_name,
+                client=client,
+                args=arg,
+                value=int(0.1 * 10**18) if fn_name == "play" else 0,
+            )
+        except Exception as e:
+            print(f"Error on call {fn_name} with args {arg}: {str(e)}")
+
+        time.sleep(5)
