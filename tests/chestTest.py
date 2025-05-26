59a5a0d0c98304c50b302c158fde3e2c75f45001
sesamehowie <spooolieyeah@gmail.com>
2025-05-26 11:51:14 +0300

    new tests

--- tests/chestTest.py
+++ tests/chestTest.py
@@ -8,7 +8,7 @@ def test_chest(client):
         interface.execute_write_function(
             function_name="withdraw",
             client=client,
-            args=[int(800 * 10**18)],
+            args=[int(900 * 10**18)],
         )
     except Exception as e:
         print(f"Error: {str(e)}")
--- tests/plinkoTest.py
+++ tests/plinkoTest.py
@@ -9,7 +9,7 @@ def test_plinko(client):
         interface.execute_write_function(
             function_name="withdraw",
             client=client,
-            args=[int(1350 * 10**18)],
+            args=[int(400 * 10**18)],
         )
     except Exception as e:
         print(f"Error: {str(e)}")
