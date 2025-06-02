dcd3606dfb4fadd83c91a1a3400e786810b0cf86
sesamehowie <spooolieyeah@gmail.com>
2025-06-02 11:30:03 +0300

    new test

--- tests/rpsTest.py
+++ tests/rpsTest.py
@@ -9,7 +9,7 @@ def test_rps(client):
         interface.execute_write_function(
             function_name="withdrawBankroll",
             client=client,
-            args=[int(200 * 10**18)],
+            args=[int(1000 * 10**18)],
         )
     except Exception as e:
         print(f"Error: {str(e)}")
