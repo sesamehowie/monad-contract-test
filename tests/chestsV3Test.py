d7b515247335ce2d5e073462e02445fcfd22dfa3
sesamehowie <spooolieyeah@gmail.com>
2025-07-03 13:27:23 +0300

    new test

--- tests/chestsV3Test.py
+++ tests/chestsV3Test.py
@@ -20,17 +20,21 @@ COMMANDS = [
 def test_chests_v3(client):
     interface = ChestsV3Interface
 
-    for command in COMMANDS:
-        try:
-            interface.execute_write_function(
-                function_name=command["name"],
-                client=client,
-                args=command["args"],
-                value=command["value"],
-                estimate_gas=False,
-            )
+    # for command in COMMANDS:
+    #     try:
+    #         interface.execute_write_function(
+    #             function_name=command["name"],
+    #             client=client,
+    #             args=command["args"],
+    #             value=command["value"],
+    #             estimate_gas=False,
+    #         )
 
-        except Exception as e:
-            print(f"Error: {str(e)}")
+    #     except Exception as e:
+    #         print(f"Error: {str(e)}")'
 
-        time.sleep(10)
+    # time.sleep(10)
+
+    interface.execute_read_function(
+        "playerWinnings", client, ["0xb66C8E76bF9B434bca77cd1B132A199D351C6bB5"]
+    )
