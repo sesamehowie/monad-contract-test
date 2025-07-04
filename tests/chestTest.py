8657fb66ad5577789c23f043596921144357e86c
sesamehowie <spooolieyeah@gmail.com>
2025-07-04 15:45:07 +0300

    new tests

--- tests/chestTest.py
+++ tests/chestTest.py
@@ -6,11 +6,14 @@ def test_chest(client):
     interface = ChestsInterface
 
     try:
-        interface.execute_write_function(
-            function_name="withdraw",
-            client=client,
-            args=[int(3000 * 10**18)],
+        interface.execute_read_function(
+            "playerWinnings", client, ["0xb66C8E76bF9B434bca77cd1B132A199D351C6bB5"]
         )
+        # interface.execute_write_function(
+        #     function_name="withdraw",
+        #     client=client,
+        #     args=[int(3000 * 10**18)],
+        # )
     except Exception as e:
         print(f"Error: {str(e)}")
 
--- tests/newChestTest.py
+++ tests/newChestTest.py
@@ -2,33 +2,21 @@ import time
 from os import urandom
 from src.interfaces.interfaces import NewChestsInterface
 
-COMMANDS = [
-    {
-        "name": "openChests",
-        "value": int(),
-        "args": [[0, 0, 0], urandom(32)],
-    },
-    {"name": "claimWinnings", "value": 0, "args": []},
-    # {"name": "withdraw", "value": 0, "args": [int(1 * 10**18)]}
-]
-
-
 def test_new_chests(client):
     interface = NewChestsInterface
 
-    for command in COMMANDS:
-        try:
-            # interface.execute_write_function(
-            #     function_name=command["name"],
-            #     client=client,
-            #     value=command["value"],
-            #     estimate_gas=False,
-            # )
-            interface.execute_read_function(
-                function_name="requests", client=client, args=[2656070]
-            )
+    try:
+        # interface.execute_write_function(
+        #     function_name=command["name"],
+        #     client=client,
+        #     value=command["value"],
+        #     estimate_gas=False,
+        # )
+        interface.execute_read_function(
+            "playerWinnings", client, ["0xb66C8E76bF9B434bca77cd1B132A199D351C6bB5"]
+        )
 
-        except Exception as e:
-            print(f"Error: {str(e)}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
         time.sleep(10)
