aa1e518f0570ed1f3fb863a1ec2ceeb37815ee6a
sesamehowie <spooolieyeah@gmail.com>
2025-08-25 14:08:53 +0300

    new test

--- tests/rouletteTest.py
+++ tests/rouletteTest.py
@@ -8,7 +8,7 @@ def test_roulette(client):
         interface.execute_write_function(
             function_name="withdraw",
             client=client,
-            args=[int(2700 * 10**18)],
+            args=[int(2300 * 10**18)],
         )
     except Exception as e:
         print(f"Error: {str(e)}")
